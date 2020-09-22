67afcd4f4e0f6ec530a97240a4ac0ce74c6693c3
JuanTorres <JuanTorresNavarro93@gmail.com>
2020-09-22 10:14:11 +0200

    Added emacs config

---
+++ .emacs.d/init.el
@@ -0,0 +1,8 @@
+; Disable menu bar, tool bar and scroll bar
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(toggle-scroll-bar -1)
+
+; Enable line numbers and relative numbers
+(global-display-line-numbers-mode 1)
+(setq display-line-numbers-type 'relative)
