34ca09a548880115845c8b9da2cd8e00b35a1f07
JuanTorres <JuanTorresNavarro93@gmail.com>
2020-09-25 21:27:34 +0200

    Modified emacs configuration

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -26,7 +26,8 @@
  '(custom-enabled-themes '(doom-dracula))
  '(custom-safe-themes
    '("e6ff132edb1bfa0645e2ba032c44ce94a3bd3c15e3929cdf6c049802cf059a2a" default))
- '(package-selected-packages '(org-ac beacon which-key use-package doom-themes)))
+ '(package-selected-packages
+   '(company dashboard rainbow-delimiters sudo-edit rainbow-mode avy smex ido-vertical-mode org-ac beacon which-key use-package doom-themes)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
