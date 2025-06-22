aea6c52bed7bc97cae4717c1f7b0467b63dbad15
Juan Torres <juan93git@gmail.com>
2025-06-22 19:26:18 +0200

    Remove time limit when editting video or writing content

--- .local/bin/edit-video.sh
+++ .local/bin/edit-video.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Open odoo in firefox (content creation project)
-firefox "https://e.ggtimer.com/30" "http://localhost:8069/web#action=194&model=project.project&view_type=kanban&cids=1&menu_id=112" "https://www.epidemicsound.com/music/featured/"  "https://chatgpt.com/c/67e7d6f6-d164-8006-a25a-af88b23030d3" &
+# firefox "https://e.ggtimer.com/30" "http://localhost:8069/web#action=194&model=project.project&view_type=kanban&cids=1&menu_id=112" "https://www.epidemicsound.com/music/featured/"  "https://chatgpt.com/c/67e7d6f6-d164-8006-a25a-af88b23030d3" &
+firefox "http://localhost:8069/web#action=194&model=project.project&view_type=kanban&cids=1&menu_id=112" "https://www.epidemicsound.com/music/featured/"  "https://chatgpt.com/c/67e7d6f6-d164-8006-a25a-af88b23030d3" &
 
 # Open kdenlive
 /usr/bin/kdenlive &
